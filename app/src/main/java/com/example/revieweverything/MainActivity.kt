be507be05eabc5291c5107be44134e3c4458c215
PetrescuAlexandru <al.ant.petrescu@gmail.com>
2023-05-01 09:24:53 +0300

    Added the styles for the text that would be used for the login & signup part of the application

--- app/src/main/java/com/example/revieweverything/MainActivity.kt
+++ app/src/main/java/com/example/revieweverything/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.revieweverything
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.revieweverything.ui.theme.BackgroundWhiteTheme
 import com.example.revieweverything.ui.theme.ReviewEverythingTheme
+import com.example.revieweverything.ui.theme.fonts
+import com.example.revieweverything.ui.theme.textWhiteTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,11 +51,77 @@ fun LoginSignupBackground(
     )
 }
 
+@Composable
+fun StandardText(
+    text: String
+) {
+    Text(
+        modifier = Modifier
+            .padding(5.dp),
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        color = textWhiteTheme,
+        text = text
+    )
+}
+
+@Composable
+fun BoldStandardText(
+    text: String
+) {
+    Text(
+        modifier = Modifier
+            .padding(5.dp),
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        color = textWhiteTheme,
+        text = text
+    )
+}
+
+@Composable
+fun LoginText(
+    text: String
+) {
+    Text(
+        modifier = Modifier
+            .padding(5.dp),
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 25.sp,
+        color = textWhiteTheme,
+        text = text
+    )
+}
+
+@Composable
+fun TitleText(
+    text: String
+) {
+    Text(
+        modifier = Modifier
+            .padding(5.dp),
+        fontFamily = fonts,
+        fontWeight = FontWeight.Black,
+        fontSize = 35.sp,
+        color = textWhiteTheme,
+        text = text
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
-fun LoginSignupBackgroundPreview(
-) {
-    LoginSignupBackground {}
+fun StandardText() {
+    LoginSignupBackground {
+        Column {
+            StandardText(text = "Hello there")
+            BoldStandardText(text = "Hello there")
+            LoginText(text = "Hello there")
+            TitleText(text = "Hello there")
+        }
+    }
 }
 
 //
--- app/src/main/java/com/example/revieweverything/ui/theme/Type.kt
+++ app/src/main/java/com/example/revieweverything/ui/theme/Type.kt
@@ -2,27 +2,33 @@ package com.example.revieweverything.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.revieweverything.R
+
+val fonts = FontFamily(
+    Font(R.font.roboto_black, weight = FontWeight.Black),
+    Font(R.font.roboto_black_italic, style = FontStyle.Italic),
+    Font(R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(R.font.roboto_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.roboto_light, weight = FontWeight.Light),
+    Font(R.font.roboto_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.roboto_light_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(R.font.roboto_medium_italic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(R.font.roboto_thin, weight = FontWeight.Thin),
+    Font(R.font.roboto_thin_italic, weight = FontWeight.Thin, style = FontStyle.Italic)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
