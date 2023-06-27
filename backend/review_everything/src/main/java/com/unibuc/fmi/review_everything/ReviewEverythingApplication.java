dc0b73dc6b43caf3007796e6ac3e097523905664
Nistor Gheorghe <nistorgeorge404@gmail.com>
2023-06-27 12:02:01 +0300

    Spring initializr

---
+++ backend/review_everything/src/main/java/com/unibuc/fmi/review_everything/ReviewEverythingApplication.java
@@ -0,0 +1,13 @@
+package com.unibuc.fmi.review_everything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewEverythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewEverythingApplication.class, args);
+	}
+
+}
