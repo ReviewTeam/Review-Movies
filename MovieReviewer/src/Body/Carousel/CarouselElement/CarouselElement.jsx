92ff19d60fadc04845a13b894022f5e69a75e4aa
PetrescuAlexandru <al.ant.petrescu@gmail.com>
2023-06-30 17:22:53 +0300

    Started working on the body part of the home page. Created the Body folder with the folders for the components of Carousel and CarouselElement. Created the CarouselElement.jsx and css file

---
+++ MovieReviewer/src/Body/Carousel/CarouselElement/CarouselElement.jsx
@@ -0,0 +1,11 @@
+import HarryPotterMovie from '../../../assets/images/HarryPotterMovie.jpg'
+
+function CarouselElement() {
+    return (
+        <div>
+            <img src={HarryPotterMovie} className="carouselElement" alt="Harry Potter Movie"/>
+        </div>
+    )
+}
+
+export default CarouselElement
