0abc626eee20bbe0149562c280e24792b5be437e
PetrescuAlexandru <al.ant.petrescu@gmail.com>
2023-07-01 11:57:57 +0300

    Changed the Carousel component and added a scroll right logo button

--- MovieReviewer/src/Body/Carousel/Carousel.jsx
+++ MovieReviewer/src/Body/Carousel/Carousel.jsx
@@ -1,16 +1,23 @@
 import CarouselElement from "../CarouselElement/CarouselElement"
+import carouselRightLogo from "../../assets/carouselRightLogo.svg"
 import './Carousel.css'
 
 function Carousel() {
-    const numOfElements = 6
+    const numOfElements = 10
     const carouselArray = Array(numOfElements).fill(null).map((_, index) => index)
     console.log(carouselArray)
 
     return (
         <div className="carousel">
-            {carouselArray.map((index) => (
-                <CarouselElement key={index} />
-            ))}
+            <section>
+                {carouselArray.map((index) => (
+                    <CarouselElement key={index} />
+                ))}
+            </section>
+            
+            <div>
+                <img src={carouselRightLogo} />
+            </div>    
         </div>
     )
 }
