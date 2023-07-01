6af711db7765d18e07eac72ea4f630066a833df0
PetrescuAlexandru <al.ant.petrescu@gmail.com>
2023-07-01 19:04:01 +0300

    Deleted a navbar that was repeated

--- MovieReviewer/src/HomePage/HomePage.jsx
+++ MovieReviewer/src/HomePage/HomePage.jsx
@@ -1,10 +1,8 @@
 import Body from "../Body/ReviewBody"
-import Navbar from "../Header/Navbar/Navbar"
 
 function HomePage() {
     return (
         <div>
-            <Navbar />
             <ReviewBody />
         </div>
     )
