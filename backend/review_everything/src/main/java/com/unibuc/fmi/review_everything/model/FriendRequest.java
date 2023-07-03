9b208fc974a3fd308ffeadab1f91520fc5156c21
IaniIspas <gabi_iani21@yahoo.com>
2023-07-04 01:40:09 +0300

    Add FriendRequest Model

---
+++ backend/review_everything/src/main/java/com/unibuc/fmi/review_everything/model/FriendRequest.java
@@ -0,0 +1,28 @@
+package com.unibuc.fmi.review_everything.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class FriendRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
---
+++ backend/review_everything/src/main/java/com/unibuc/fmi/review_everything/model/Status.java
@@ -0,0 +1,7 @@
+package com.unibuc.fmi.review_everything.model;
+
+public enum Status {
+    ACCEPTED,
+    REJECTED,
+    WAITING
+}
