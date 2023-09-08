4a15ce4c0fff3fae398fd2eb244f7b0b3e663a5b
Cosmin Colceru <cosmincolceru@gmail.com>
2023-09-08 16:47:53 +0300

    Added feed to home page

--- MovieReviewer/src/Body/Feed/Feed.jsx
+++ MovieReviewer/src/Body/Feed/Feed.jsx
@@ -1,5 +1,8 @@
 import Review from "../Review/Review";
+import Review1 from "../../Movie/MovieReview";
 import "./Feed.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Feed({
   searchValue,
@@ -11,10 +14,19 @@ function Feed({
   // console.log("array of reviews")
   // console.log(arrayReviews)
   console.log("Search revieweul state-ul este: ", searchedReviewsState);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    // get the logged in user
+    axios.get("http://localhost:8080/api/v1/feed").then((response) => {
+      setReviews(response.data);
+    });
+  }, []);
+  console.log(reviews);
 
   return (
     <div className="feed">
-      {searchValue
+      {/* {searchValue
         ? searchedReviewsState.map((review, id) => {
             const newObject = {
               ...review,
@@ -26,7 +38,11 @@ function Feed({
         : reviewsState.map((review, id) => {
             const newObject = { ...review, reviewsState, setReviewsState };
             return <Review key={id} id={id} {...newObject} />;
-          })}
+          })} */}
+
+      {reviews.map((review) => (
+        <Review1 id={review.id}></Review1>
+      ))}
 
       {/* <h3>Add new review</h3>
             
